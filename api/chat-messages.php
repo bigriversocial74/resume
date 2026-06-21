0ca1cc0e1780a41ac8a0a1026c2f9198a032c3c4
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-21 16:12:02 -0700

    Mark visitor chat reads during polling

--- api/chat-messages.php
+++ api/chat-messages.php
@@ -12,7 +12,8 @@ try {
         echo json_encode(['ok' => false]);
         exit;
     }
-    echo json_encode(['ok' => true, 'messages' => read_chat_messages($conversationId)]);
+    mark_chat_read_for_visitor($conversationId);
+    echo json_encode(['ok' => true, 'messages' => read_chat_messages($conversationId), 'unread' => 0]);
 } catch (Throwable $e) {
     http_response_code(400);
     echo json_encode(['ok' => false]);
